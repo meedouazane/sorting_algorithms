76766b006808f784bfd3a895d9435e0a88f5968d
meedouazane <mohamedouazane@yahoo.com>
2023-10-19 13:54:58 -0700

    01

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a doubly linked list using the Insertion sort algorithm.
+ * @list: linked list of int
+ * Return: Nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+
---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+/**
+ * selection_sort - function that sorts an array using the Selection sort
+ * @array: an array of integers
+ * @size: size of the array
+ * Return: Nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, z = 0;
+	size_t tmp, min = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		}
+		if (min != i)
+		{
+		tmp = array[i];
+		array[i] = array[min];
+		array[min] = tmp;
+		for (z = 0; z < size ; z++)
+		{
+			if (z == size - 1)
+				printf("%d", array[z]);
+			else
+				printf("%d ,", array[z]);
+		}
+		printf("\n");
+		}
+	}
+}
+
