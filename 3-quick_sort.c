29b28daedb7a793bff05588b9b4a4614bf54845f
meedouazane <mohamedouazane@yahoo.com>
2023-10-20 15:16:20 -0700

    3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,16 +4,15 @@
  * @array: array of int
  * @low: first element in an array
  * @high: last element in an array
+ * @size: size of array
  * Return: the index of element pivot in array
  */
-int partition(int *array, int low, int high)
+int partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
 	int i = low - 1;
 	int tmp, j = 0;
-	size_t size = 0;
 
-	size = sizeof(array) + 2;
 	for (j = low; j <= high; j++)
 	{
 		if (array[j] < pivot)
@@ -33,19 +32,20 @@ int partition(int *array, int low, int high)
 /**
  * quick_sort_helper - first part of quick sort
  * @array: array of int
+ * @size: size of array
  * @low: first element in an array
  * @high: last element in an array
  * Return: Nothing
  */
-void quick_sort_helper(int *array, int low, int high)
+void quick_sort_helper(int *array, int low, int high, size_t size)
 {
 	int part = 0;
 
 	if (low < high)
 	{
-		part = partition(array, low, high);
-		quick_sort_helper(array, low, part - 1);
-		quick_sort_helper(array, part + 1, high);
+		part = partition(array, low, high, size);
+		quick_sort_helper(array, low, part - 1, size);
+		quick_sort_helper(array, part + 1, high, size);
 	}
 }
 /**
@@ -56,11 +56,8 @@ void quick_sort_helper(int *array, int low, int high)
  */
 void quick_sort(int *array, size_t size)
 {
-	int high, low = 0;
-
-	if (!array || !size)
-		return;
+	int high = 0;
 
 	high = size - 1;
-	quick_sort_helper(array, low, high);
+	quick_sort_helper(array, 0, high, size);
 }
